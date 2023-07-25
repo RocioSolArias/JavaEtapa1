96208d619e7c3cab87a3014c1071894f11d0d14e
Rocio <rocio.sol.arias@gmail.com>
2023-07-25 02:13:29 -0300

    Primer Commit

---
+++ src/javaintro01/JavaIntro01.java
@@ -0,0 +1,25 @@
+/*
+Este programa escribe el texto "Hola Mundo" en la consola, 
+utilizando el método System.out.println
+ */
+package primerprograma;
+//package <nombre_paq_sup>[.<nombre_sub_paq>]*;
+
+/**
+ *
+ * @author admi
+ */
+public class HolaMundo {
+//Declaración de clase {
+//Cuerpo de clase
+    /**
+     * @param args the command line arguments
+     */    
+    public static void main(String[] args) { 
+        
+    //La línea 12 muestra el mensaje por pantalla al ejecutar el programa
+    
+    System.out.println("Hola mundo");
+    }
+    
+}
